ffa6d59ac70779c404fc0d4108db4497123382b5
AlyonaTiki <31675522+AlyonaTiki@users.noreply.github.com>
2018-03-19 18:32:16 -0400

    index.html and array in js

---
+++ app.js
@@ -0,0 +1,139 @@
+const ourQuestions = [
+  {
+    question: "Was Kishimoto the creator of Naruto?",
+    img: "img/1.jpg",
+    answers: {
+      a: "true",
+      b: "false"
+    },
+    correctAnswer: "a"
+  },
+  {
+    question: "Is goku from Howl's moving Castle?",
+    img: "img/1.jpg",
+    answers: {
+      a: "true",
+      b: "false"
+    },
+    correctAnswer: "b"
+  },
+  {
+    question: "Tide Kubo was the Creator of Bleach?",
+    img: "img/1.jpg",
+    answers: {
+      a: "true",
+      b: "false"
+    },
+    correctAnswer: "a"
+   },
+   {
+    question: "Did Hayao Miyazaki Created Spirited away?",
+    img: "img/1.jpg",
+    answers: {
+      a: "true",
+      b: "false"
+    },
+    correctAnswer: "a"
+   },
+   {
+    question: "Sailor Mars from from the Show Terraformars?",
+    img: "img/1.jpg",
+    answers: {
+      a: "true",
+      b: "false"
+    },
+    correctAnswer: "b"
+   },
+   {
+    question: "Was Sasuke the main Character of the show Naruto?",
+    img: "img/1.jpg",
+    answers: {
+      a: "true",
+      b: "false"
+    },
+    correctAnswer: "b"
+   },
+   {
+    question: "Was the The character Totoro in the Movie kiki Deliverys service?",
+    img: "img/1.jpg",
+    answers: {
+      a: "true",
+      b: "false"
+    },
+    correctAnswer: "b"
+   },
+   {
+    question: "In the movie My Neighbor Totoro a Dog shaped bus stopped at the bus stop?",
+    img: "img/1.jpg",
+    answers: {
+      a: "true",
+      b: "false"
+    },
+    correctAnswer: "b"
+   },
+   {
+    question: "Gundam wing was the best Gundam?(There's only ONE RIGHT ANSWER!..)?",
+    img: "img/1.jpg",
+    answers: {
+      a: "true",
+      b: "false"
+    },
+    correctAnswer: "a"
+   },
+   {
+    question: "In The show cowboy bebop is the Character Ed a Male?",
+    img: "img/1.jpg",
+    answers: {
+      a: "true",
+      b: "false"
+    },
+    correctAnswer: "b"
+   }
+    ]
+
+
+
+
+
+
+for(var i=0; i<questions.length; i++){
+
+	var userAnswer = " ";
+	var numCorrect = 0;
+		// if answer is correct
+		if(userAnswer==questions[i].correctAnswer){
+			// add to the number of correct answers
+			numCorrect++;
+			
+        }
+
+var a2 = document.getElementsById('q2');
+   for(i = 0; i < a3.length; i++) {
+      if(a3[i].checked) {
+         if(a3[i].value == 'augusta') {
+            correctAnswers++;
+            break;
+         }
+      }
+   }
+    
+//const t1Button = document.getElementById("true1");
+//const f1Button = document.getElementById("false1");
+//const t2Button = document.getElementById("true2");
+//const f2Button = document.getElementById("false2");
+//const t3Button = document.getElementById("true3");
+//const f3Button = document.getElementById("false3");
+//const t4Button = document.getElementById("true4");
+//const f4Button = document.getElementById("false4");
+//const t5Button = document.getElementById("true5");
+//const f5Button = document.getElementById("false5");
+//const t6Button = document.getElementById("true6");
+//const f6Button = document.getElementById("false6");
+//const t7Button = document.getElementById("true7");
+//const f7Button = document.getElementById("false7");
+//const t8Button = document.getElementById("true8");
+//const f8Button = document.getElementById("false8");
+//const t9Button = document.getElementById("true9");
+//const f9Button = document.getElementById("false9");
+//const t10Button = document.getElementById("true10");
+//const f10Button = document.getElementById("false10");
