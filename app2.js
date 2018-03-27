bcdb2a2e53d1e7c6afb86b5aaaa1f49c59fcb0d8
AlyonaTiki <31675522+AlyonaTiki@users.noreply.github.com>
2018-03-26 21:07:10 -0400

    counter works

--- app2.js
+++ app2.js
@@ -1,4 +1,3 @@
-
 //for(var i=0; i<question.length; i++){
 //var q = myQuestions[questionIndex];
 //q.textContent = q.question;
@@ -10,9 +9,9 @@
 
 //array of images
 const images = [
-	"images/Q1pic.jpg", 
-	"images/Q2pic.jpg", 
-	"images/Q3pic.png", 
+	"images/Q1pic.jpg",
+	"images/Q2pic.jpg",
+	"images/Q3pic.png",
 	"images/Q4pic.jpg",
 	"images/Q5pic.jpg",
 	"images/Q6pic.jpg",
@@ -25,99 +24,126 @@ const images = [
 //array of questions and answers
 const questions = [
 
-	{
-	    problem: "1) Was Kishimoto the creator of Naruto?",
-	    option:{A:true, B:false},
-	    answer:'true'
+    {
+        problem: "1) Was Kishimoto the creator of Naruto?",
+        option: {
+            A: "true",
+            B: "false"
+        },
+        answer: "true"
 	},
-	{
-	    problem: "2) Is goku from Howl's moving Castle?",
-	    option:{A:true, B:false},
-	    answer:''
+    {
+        problem: "2) Is goku from Howl's moving Castle?",
+        option: {
+            A: "true",
+            B: "false"
+        },
+        answer: "true"
 	},
-	{
-		problem: "3) Tide Kubo was the Creator of Bleach?",
-	    option:{A:true, B:false},
-	    answer:''
+    {
+        problem: "3) Tide Kubo was the Creator of Bleach?",
+        option: {
+            A: "true",
+            B: "false"
+        },
+        answer: "false"
 	},
-	{
-		problem: "4) Did Hayao Miyazaki Created Spirited away?",
-	    option:{A:true, B:false},
-	    answer:''
+    {
+        problem: "4) Did Hayao Miyazaki Created Spirited away?",
+        option: {
+            A: "true",
+            B: "false"
+        },
+        answer: ''
 	},
-	{
-		problem: "5) Sailor Mars from from the Show Terraformars?",
-	    option:{A:true, B:false},
-	    answer:''
+    {
+        problem: "5) Sailor Mars from from the Show Terraformars?",
+        option: {
+            A: "true",
+            B: "false"
+        },
+        answer: ''
 	},
-	{
-		problem: "6) In the show Naruto Shippuden, Tobi is secretly Madara?",
-	    option:{A:true, B:false},
-	    answer:''
+    {
+        problem: "6) In the show Naruto Shippuden, Tobi is secretly Madara?",
+        option: {
+            A: "true",
+            B: "false"
+        },
+        answer: ''
 	},
-	{
-		problem: "7) Was the The character Totoro in the Movie kiki Deliverys service?",
-	    option:{A:true, B:false},
-	    answer:''
+    {
+        problem: "7) Was the The character Totoro in the Movie kiki Deliverys service?",
+        option: {
+            A: "true",
+            B: "false"
+        },
+        answer: ''
 	},
-	{
-		problem: "8) In the movie My Neighbor Totoro a Dog shaped bus stopped at the bus stop?",
-	    option:{A:true, B:false},
-	    answer:''
+    {
+        problem: "8) In the movie My Neighbor Totoro a Dog shaped bus stopped at the bus stop?",
+        option: {
+            A: "true",
+            B: "false"
+        },
+        answer: ''
 	},
-	{
-		problem: "9) Gundam wing was the best Gundam?(There's only ONE RIGHT ANSWER!..)?",
-	    option:{A:true, B:false},
-	    answer:''
+    {
+        problem: "9) Gundam wing was the best Gundam?(There's only ONE RIGHT ANSWER!..)?",
+        option: {
+            A: "true",
+            B: "false"
+        },
+        answer: ''
 	},
-	{
-		problem: "10) In The show cowboy bebop is the Character Ed a Male?",
-	    option:{A:true, B:false},
-	    answer:''
+    {
+        problem: "10) In The show cowboy bebop is the Character Ed a Male?",
+        option: {
+            A: "true",
+            B: "false"
+        },
+        answer: ''
 	}
 ];
-
-
-
 
 //display the questions and images
 
 const container = document.getElementById("container");
-
 for (let i = 0; i < images.length; i++) {
-	
-	const imgContainer = document.createElement('div');
-	
-	const problems = document.createElement('p');
-	problems.classList.add("problem");
-	problems.textContent = questions[i].problem;
-	imgContainer.appendChild(problems);
-		
-	const img = new Image();
-	img.src = images[i];
-	img.id = "img"+i;
-	img.classList.add("slide");
-	imgContainer.appendChild(img);
-    
-	console.log(questions[i].option);
+
+    const imgContainer = document.createElement('div');
+
+    const problems = document.createElement('p');
+    problems.classList.add("problem");
+    problems.textContent = questions[i].problem;
+    imgContainer.appendChild(problems);
+
+    const img = new Image();
+    img.src = images[i];
+    img.id = "img" + i;
+    img.classList.add("slide");
+    imgContainer.appendChild(img);
+
     for (let key in questions[i].option) {
-       let option = questions[i].option[key];
-	   const options = document.createElement('button');
-	   options.classList.add("option");
-	   options.textContent = option;
-       var counter = 0;
-       options.onclick = function() {
-       options.textContent = option;
-        if (questions.option == questions[i].answer.textContent) {
-            counter++;
-        }   
-           console.log(counter);
+        let option = questions[i].option[key];
+        const options = document.createElement('button');
+        options.classList.add("option");
+        options.textContent = option;
+        var counter = 0;
+
+        options.onclick = function () {
+//            console.log(questions[i].answer);
+//            console.log(this.textContent);
+            if (questions[i].answer == this.textContent) {
+                counter++;
+            }
+            console.log(counter);
         };
         imgContainer.appendChild(options);
     }
-	
-    
 
-	container.appendChild(imgContainer);
-	
+
+
+    container.appendChild(imgContainer);
+
 }
